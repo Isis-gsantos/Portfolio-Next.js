baaafeb3d7d182c826f63fd4c07179a71526063c
Isis Gonçalves Santos <isissantos8425@gmail.com>
2024-06-18 15:39:28 -0300

    editing next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 import createNextIntlPlugin from 'next-intl/plugin';
- 
+
 const withNextIntl = createNextIntlPlugin();
- 
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
- 
+
 export default withNextIntl(nextConfig);
